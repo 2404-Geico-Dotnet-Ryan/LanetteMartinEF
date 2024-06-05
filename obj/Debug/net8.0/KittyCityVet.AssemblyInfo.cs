8168ef5d004ae426f516c2ae7988089ac73b0f6c
Lanette Martin <lilcow@yahoo.com>
2024-06-05 17:54:44 -0400

    June5Updates

--- obj/Debug/net8.0/KittyCityVet.AssemblyInfo.cs
+++ obj/Debug/net8.0/KittyCityVet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KittyCityVet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+026061e4d5693b41c282ab7f24f86b77cd8900ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c4c224455323659fac815168ecaa7dbbcb5bd2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("KittyCityVet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KittyCityVet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
